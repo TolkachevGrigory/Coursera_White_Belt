60dcf6a7548cd6459ee08da991cf144bfdfa9144
TolkachevGrigory <55763195+TolkachevGrigory@users.noreply.github.com>
2020-08-16 16:21:29 +0300

    Add files via upload

---
+++ week4/task2.cpp
@@ -0,0 +1,70 @@
+//
+//  week4task2
+//  Реализуйте рассказанный на лекции класс Function, позволяющий создавать, вычислять и инвертировать функцию,
+//  состоящую из следующих элементарных операций:
+//  прибавить вещественное число x;
+//  вычесть вещественное число x.
+//  При этом необходимо объявить константными все методы, которые по сути такими являются.
+//
+//
+
+#include <iostream>
+#include <string>
+#include <math.h>
+#include <map>
+#include <vector>
+#include <set>
+#include <algorithm>
+#include <cctype>
+
+using namespace std;
+class FunctionPart{
+public:
+    FunctionPart(char new_operation, double new_value){
+        operation = new_operation;
+        value = new_value;
+    }
+    
+    double Apply(double source_value) const{
+        if(operation == '+'){
+            return source_value + value ;
+        }else{
+            return source_value - value;
+        }
+    }
+    
+    void Invert(){
+        if(operation == '+'){
+            operation = '-';
+        }else{
+            operation = '+';
+        }
+    }
+    
+private:
+    char operation;
+    double value;
+};
+
+class Function{
+public:
+    void AddPart(char operation, double value){
+        parts.push_back({operation, value});
+    }
+    double Apply(double value) const{
+        for(const FunctionPart& part: parts){
+            value = part.Apply(value);
+        }
+        return value;
+    }
+    
+    void Invert(){
+        for( FunctionPart& part: parts){
+            part.Invert();
+        }
+        reverse(begin(parts), end(parts));
+    }
+private:
+    vector<FunctionPart> parts;
+};
+
---
+++ week4/task3.cpp
@@ -0,0 +1,74 @@
+//
+//  week4task3
+//  Добавьте в класс Function из задачи «Обратимая функция» обработку умножения ('*') и деления ('/').
+//  Гарантируется отсутствие элементарных операций умножения и деления на 0.
+
+
+#include <iostream>
+#include <string>
+#include <math.h>
+#include <map>
+#include <vector>
+#include <set>
+#include <algorithm>
+#include <cctype>
+
+using namespace std;
+class FunctionPart{
+public:
+    FunctionPart(char new_operation, double new_value){
+        operation = new_operation;
+        value = new_value;
+    }
+    
+    double Apply(double source_value) const{
+        if(operation == '+'){
+            return source_value + value ;
+        }else if (operation == '-'){
+            return source_value - value;
+        }else if(operation == '*'){
+            return source_value*value;
+        }else{
+            return source_value/value;
+        }
+    }
+    
+    void Invert(){
+        if(operation == '+'){
+            operation = '-';
+        }else if (operation == '-'){
+            operation = '+';
+        }else if(operation == '*'){
+            operation = '/';
+        }else{
+            operation = '*';
+        }
+    }
+    
+private:
+    char operation;
+    double value;
+};
+
+class Function{
+public:
+    void AddPart(char operation, double value){
+        parts.push_back({operation, value});
+    }
+    double Apply(double value) const{
+        for(const FunctionPart& part: parts){
+            value = part.Apply(value);
+        }
+        return value;
+    }
+    
+    void Invert(){
+        for( FunctionPart& part: parts){
+            part.Invert();
+        }
+        reverse(begin(parts), end(parts));
+    }
+private:
+    vector<FunctionPart> parts;
+};
+
